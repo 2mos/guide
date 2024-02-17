2b1358c3bda33b424f713a1a04a6cb234c003004
mohit <157024103+moss0@users.noreply.github.com>
2024-02-18 01:57:01 +0530

    dark theme update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,14 @@ module.exports = {
   theme: {
     extend: {
       typography: ({ theme }) => ({
-        "stone-dark": {
+        "neutral-dark": {
           css: {
-            "--tw-prose-body": theme("colors.stone.100"),
+            "--tw-prose-body": theme("colors.neutral.100"),
             "--tw-prose-headings": theme("colors.white"),
-            "--tw-prose-pre-bg": theme("colors.stone.800"),
+            "--tw-prose-pre-bg": theme("colors.neutral.800"),
             "--tw-prose-links": theme("colors.blue[600]"),
             "--tw-prose-bold": theme("colors.cyan[300]"),
-            backgroundColor: theme("colors.stone.900"),
+            backgroundColor: theme("colors.neutral.900"),
           },
         },
       }),
